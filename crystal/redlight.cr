9832d78878c5aec037b6599baff1d9385a736092
DumbMahreeo <43857998+DumbMahreeo@users.noreply.github.com>
2021-05-16 16:35:59 +0200

    Adding redlight.cr

---
+++ crystal/redlight.cr
@@ -0,0 +1,19 @@
+#!/usr/bin/crystal
+
+# This should be compiled with the Crystal compiler.
+# The shebang is here just to be sure. Please don't directly execute this file.
+
+if ARGV.size != 0
+    ARGV[0].to_i?
+
+    case ARGV[0]
+
+    when "on" then `redshift -PO 5000`
+    when "off" then `redshift -x`
+
+    else
+        arg = ARGV[0].to_i?
+        `redshift -PO #{arg}` if arg != nil
+
+    end
+end
